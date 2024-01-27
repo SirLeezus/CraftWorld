f838f23241716988d35d96ac6be7b1dd64991212
Lee <leecook808@gmail.com>
2024-01-26 16:59:04 -0800

    Initial commit

---
+++ Assets/_Scripts/Trees/TreeData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeData
+{
+    public List<Vector2Int> treePositions = new List<Vector2Int>();
+    public List<Vector3Int> treeLeafesSolid = new List<Vector3Int>();
+    public List<Vector3Int> treeLeafesTransparent = new List<Vector3Int>();
+}
